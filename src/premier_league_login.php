4baa25043d5eaf9e05dee85b0d10a02bbb9d0bbf
mfeumbaange-jpg <mfeumbaange@gmail.com>
2026-02-14 02:18:21 +0100

    Update premier_league_login.php
    Ajout de la fonction de connexion simple dans premier_league_login.php

--- src/premier_league_login.php
+++ src/premier_league_login.php
@@ -1,1 +1,28 @@
+<?php
+function login($username, $password) {
+    $users = [
+        "admin" => "admin123",
+        "user1" => "password1"
+    ];
+
+    if (isset($users[$username]) && $users[$username] === $password) {
+        session_start();
+        $_SESSION['username'] = $username;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    if (login($username, $password)) {
+        echo "Connexion réussie ! Bienvenue, $username.";
+    } else {
+        echo "Nom d'utilisateur ou mot de passe incorrect.";
+    }
+}
+?>
 
